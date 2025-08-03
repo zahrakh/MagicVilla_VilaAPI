96114cb3095c955a11c19d4b97f78dd3c90f9b4d
zahra.khodabakhshi <zahra.khodabakhshi527@gmail.com>
2025-08-03 21:23:57 +0200

    Add utility class project

---
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,10 @@
+using static MagicVilla_Utility.SD;
+ 
+namespace MagicVilla_Web.Models;
+
+public class APIRequest
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public Object Data { get; set; }
+}

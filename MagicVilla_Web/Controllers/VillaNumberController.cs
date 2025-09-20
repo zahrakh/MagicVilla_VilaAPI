6d63b72d08044d58fbc6dea007ed4ad00a47f01d
zahra.khodabakhshi <zahra.khodabakhshi527@gmail.com>
2025-09-20 19:08:04 +0200

    Add create artwork form

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.VM;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers;
@@ -10,13 +12,21 @@ namespace MagicVilla_Web.Controllers;
 public class VillaNumberController : Controller
 {
     private readonly IVillaNumberService _villaNumberService;
-    private readonly IMapper _Mapper;
-    private readonly ILogger _Logger;
+    private readonly IVillaService _villaService;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
 
-    public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper, ILogger<VillaNumberController> logger) {
+    public VillaNumberController(
+        IVillaNumberService villaNumberService,
+        IVillaService villaService,
+        ILogger<VillaNumberController> logger,
+        IMapper mapper
+    )
+    {
         _villaNumberService = villaNumberService;
-        _Mapper = mapper;
-        _Logger = logger;
+        _villaService = villaService;
+        _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<IActionResult> IndexVillaNumber()
@@ -25,41 +35,54 @@ public class VillaNumberController : Controller
         var response = await _villaNumberService.GetAllSync<APIResponse>();
         if (response.IsSuccess)
         {
-            list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result)); 
+            list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result));
             //Todo check the syntax
         }
+
         return View(list);
     }
 
-    public async Task<IActionResult> Create()
+    public async Task<IActionResult> CreateVillaNumber() 
     {
-        return View();
+        VillaNumberCreateVM villaNumberVM = new();
+        var response = await _villaService.GetAllSync<APIResponse>();
+        if (response.IsSuccess)
+        {
+            villaNumberVM.VillaList = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result)).Select(it=>new SelectListItem
+            {
+                Text = it.Name,
+                Value = it.Id.ToString()
+            });
+        }
+        return View(villaNumberVM);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> CreateVillaNumber(VillaNumberCreateDTO model)
+    public async Task<IActionResult> CreateVillaNumber(VillaNumberCreateVM model)
     {
         if (ModelState.IsValid)
         {
-            var response = await _villaNumberService.CreateSync<APIResponse>(model);
+            var response = await _villaNumberService.CreateSync<APIResponse>(model.VillaNumber);
             if (response.IsSuccess)
             {
                 return RedirectToAction(nameof(IndexVillaNumber)); //Todo alternative syntax
             }
         }
+
         return View(model);
     }
 
     public async Task<IActionResult> UpdateVillaNumber(int villaNo)
     {
-        _Logger.Log(LogLevel.Information, villaNo.ToString(), "Updating villa number");
+        _logger.Log(LogLevel.Information, villaNo.ToString(), "Updating villa number");
         var response = await _villaNumberService.GetSync<APIResponse>(villaNo);
-        if (response.IsSuccess&& response!=null)
+        if (response.IsSuccess && response != null)
         {
             VillaNumberDTO model = JsonConvert.DeserializeObject<VillaNumberDTO>(Convert.ToString(response.Result));
-            return View(_Mapper.Map<VillaUpdateDTO>(model));
+            return View(_mapper.Map<VillaUpdateDTO>(model));
         }
+
         return NotFound();
     }
 
@@ -75,7 +98,8 @@ public class VillaNumberController : Controller
                 return RedirectToAction(nameof(IndexVillaNumber));
             }
         }
-        return View(model);//todo add break point and debug why we should return the UI?
+
+        return View(model); //todo add break point and debug why we should return the UI?
     }
 
     public async Task<IActionResult> DeleteVillaNumber(int villaNo)
@@ -86,6 +110,7 @@ public class VillaNumberController : Controller
             VillaNumberDTO model = JsonConvert.DeserializeObject<VillaNumberDTO>(Convert.ToString(response.Result));
             return View(model);
         }
+
         return NotFound();
     }
 
@@ -98,8 +123,7 @@ public class VillaNumberController : Controller
         {
             return RedirectToAction(nameof(IndexVillaNumber));
         }
+
         return View(model);
     }
-
-
 }
